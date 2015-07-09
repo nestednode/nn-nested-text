66eeb595a40abfef472e35cfbe13aa7de46b5e54
anotherworkman <anotherworkman@gmail.com>
2015-07-09 19:18:26 +0300

    TextInputComponent / input event walkaround for ie

--- lib/TextInputComponent.d.ts
+++ lib/TextInputComponent.d.ts
@@ -7,11 +7,14 @@ declare module TextInputComponent {
         onBlur?: () => void;
     }
     class Component extends React.Component<Props, {}, {}> {
+        domNode: HTMLElement;
+        mutationObserver: any;
         constructor(props: any, context: any);
         protected render(): React.ReactElement;
-        private handleInput(e);
+        private handleInput();
         protected shouldComponentUpdate(nextProps: Props): boolean;
         protected componentDidMount(): void;
+        protected componentWillUnmount(): void;
     }
     function Element(props: Props): React.ReactElement;
 }
--- src/TextInputComponent.ts
+++ src/TextInputComponent.ts
@@ -1,6 +1,8 @@
 import React = require('bower_components/nn-react/React');
 import dom = React.DOM;
 
+declare var MutationObserver;
+
 
 module TextInputComponent {
 
@@ -13,6 +15,9 @@ module TextInputComponent {
 
     export class Component extends React.Component<Props, {}, {}> {
 
+        domNode: HTMLElement;
+        mutationObserver;
+
         constructor(props, context) {
             super(props, context);
             this.handleInput = this.handleInput.bind(this);
@@ -22,41 +27,62 @@ module TextInputComponent {
             return dom['div']({
                 className: this.props.className,
                 contentEditable: true,
-                onInput: this.handleInput,
                 onBlur: this.props.onBlur,
                 dangerouslySetInnerHTML: { __html: this.props.value }
             })
         }
 
-        private handleInput(e) {
-            this.props.onChange && this.props.onChange(e.target.textContent);
+        private handleInput() {
+            // при copy/paste появляются левые span-элементы,
+            // но дерево лучше не менять, иначе потрется undo history в contenteditable
+            //if (this.domNode.children.length > 0) {
+            //    this.domNode.innerHTML = this.domNode.textContent;
+            //    return; //
+            //}
+            this.props.onChange && this.props.onChange(this.domNode.textContent);
         }
 
         protected shouldComponentUpdate(nextProps: Props) {
-            var domNode = React.findDOMNode(this);
             // в firefox курсор у contenteditable сбивается при setInnerHTML,
             // поэтому компонент обновляем полностью только при рассинхронизации value
-            if (domNode.textContent != nextProps.value) {
+            if (this.domNode.textContent != nextProps.value) {
                 return true;
             }
             // не будем обновлять весь компонент, если изменилось только имя класса
-            if (domNode.className !== nextProps.className) {
-                domNode.className = nextProps.className;
+            if (this.domNode.className !== nextProps.className) {
+                this.domNode.className = nextProps.className;
             }
             return false;
             // каков бы не был результат, this.props и this.state react обновит и так
         }
 
         protected componentDidMount() {
-            var domNode = React.findDOMNode(this);
+            this.domNode = React.findDOMNode(this);
             // перемещаем курсор в конец строки
             var range = document.createRange();
-            range.selectNodeContents(domNode);
+            range.selectNodeContents(this.domNode);
             range.collapse(false);
             var selection = window.getSelection();
             selection.removeAllRanges();
             selection.addRange(range);
-            domNode.focus();
+            this.domNode.focus();
+            // ie11 не отрабатывает input event
+            // а в safari observe-событие приходит только после отпускания кнопки на клавиатуре,
+            // остальные могут и так, и эдак
+            if ('ActiveXObject' in window) {
+                this.mutationObserver = new MutationObserver(this.handleInput);
+                this.mutationObserver.observe(this.domNode, { subtree: true, childList: true, characterData: true });
+            } else {
+                this.domNode.addEventListener('input', this.handleInput);
+            }
+        }
+
+        protected componentWillUnmount() {
+            if ('ActiveXObject' in window) {
+                this.mutationObserver.disconnect();
+            } else {
+                this.domNode.removeEventListener('input', this.handleInput);
+            }
         }
 
     }
